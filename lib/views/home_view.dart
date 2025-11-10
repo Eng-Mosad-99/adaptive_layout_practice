9bc61b2351931b66dbc39e21df27983ef308e5fd
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-11-10 12:47:30 +0200

    create and open drawer

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_drawer.dart';
 import 'widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: const Color(0xffDBDBDB),
+      drawer: const CustomDrawer(),
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.white,
+        leading: GestureDetector(
+          onTap: (){
+            scaffoldKey.currentState!.openDrawer();
+          },
+          child: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
         ),
       ),
       body:const HomeViewBody(),
